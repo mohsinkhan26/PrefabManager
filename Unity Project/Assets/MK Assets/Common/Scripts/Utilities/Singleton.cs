66176bae4bbb745337022a60a4bfaf7a6f4e4d64
Wolverine <mohsinkhan.se@gmail.com>
2021-04-03 04:14:30 +0800

    unity project upgraded to 2020

--- Unity Project/Assets/MK Assets/Common/Scripts/Utilities/Singleton.cs
+++ Unity Project/Assets/MK Assets/Common/Scripts/Utilities/Singleton.cs
@@ -1,8 +1,8 @@
 ﻿/* 
  * Author : Mohsin Khan
+ * Portfolio : http://mohsinkhan26.github.io/ 
  * LinkedIn : http://pk.linkedin.com/in/mohsinkhan26/
  * Github : https://github.com/mohsinkhan26/
- * BitBucket : https://bitbucket.org/mohsinkhan26/ 
 */
 using UnityEngine;
 
